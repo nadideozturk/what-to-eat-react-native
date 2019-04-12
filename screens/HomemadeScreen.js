42bc6388d5298e8f7afa3c31db5a559e6b4efa1e
nadideozturk <nadidebaca@gmail.com>
2019-04-11 23:21:35 -0700

    Demo using react-native-grid-list

--- screens/HomemadeScreen.js
+++ screens/HomemadeScreen.js
@@ -9,180 +9,47 @@ import {
   View,
 } from 'react-native';
 import { WebBrowser } from 'expo';
+import GridList from 'react-native-grid-list';
 
 import { MonoText } from '../components/StyledText';
 
+const items = [
+  { thumbnail: { uri: 'https://res.cloudinary.com/dv0qmj6vt/image/upload/v1535268790/whattoeat/homemademeals/6192f280-b3db-4fd4-8b12-fa14a71b564a.png' } },
+  { thumbnail: { uri: 'https://res.cloudinary.com/dv0qmj6vt/image/upload/v1535271844/whattoeat/homemademeals/df354283-c92f-4e85-b759-7118b465d453.png' } },
+  { thumbnail: { uri: 'https://res.cloudinary.com/dv0qmj6vt/image/upload/v1536395221/whattoeat/homemademeals/41fb77fb-3475-4b4a-b1d1-197780bed424.png' } },
+  { thumbnail: { uri: 'https://res.cloudinary.com/dv0qmj6vt/image/upload/v1536649238/whattoeat/homemademeals/7e90512d-a70c-4d2f-8714-6665fad8de27.png' } },
+  { thumbnail: { uri: 'https://res.cloudinary.com/dv0qmj6vt/image/upload/v1536649322/whattoeat/homemademeals/577661c9-28c8-42ec-b8ee-2c13ce8a9b0d.png' } },
+  { thumbnail: { uri: 'https://res.cloudinary.com/dv0qmj6vt/image/upload/v1536650711/whattoeat/homemademeals/da5c2462-9cab-4dcc-a627-2453fac118f3.png' } },
+  { thumbnail: { uri: 'https://res.cloudinary.com/dv0qmj6vt/image/upload/v1536648960/whattoeat/homemademeals/8a1566a1-f8a4-4be7-a98d-b7e64cb11854.png' } },
+];
+
 export default class HomemadeScreen extends React.Component {
-  static navigationOptions = {
-    header: null,
-  };
+  renderItem = ({ item, index }) => (
+    <Image style={styles.image} source={item.thumbnail} />
+  );
 
   render() {
     return (
       <View style={styles.container}>
-        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-          <View style={styles.welcomeContainer}>
-            <Image
-              source={
-                __DEV__
-                  ? require('../assets/images/robot-dev.png')
-                  : require('../assets/images/robot-prod.png')
-              }
-              style={styles.welcomeImage}
-            />
-          </View>
-
-          <View style={styles.getStartedContainer}>
-            {this._maybeRenderDevelopmentModeWarning()}
-
-            <Text style={styles.getStartedText}>Get started by opening</Text>
-
-            <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-              <MonoText style={styles.codeHighlightText}>screens/HomeScreen.js</MonoText>
-            </View>
-
-            <Text style={styles.getStartedText}>
-              Change this text and your app will automatically reload.
-            </Text>
-          </View>
-
-          <View style={styles.helpContainer}>
-            <TouchableOpacity onPress={this._handleHelpPress} style={styles.helpLink}>
-              <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
-            </TouchableOpacity>
-          </View>
-        </ScrollView>
-
-        <View style={styles.tabBarInfoContainer}>
-          <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
-
-          <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-            <MonoText style={styles.codeHighlightText}>navigation/MainTabNavigator.js</MonoText>
-          </View>
-        </View>
+        <GridList
+          showSeparator
+          data={items}
+          numColumns={3}
+          renderItem={this.renderItem}
+        />
       </View>
     );
   }
-
-  _maybeRenderDevelopmentModeWarning() {
-    if (__DEV__) {
-      const learnMoreButton = (
-        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
-          Learn more
-        </Text>
-      );
-
-      return (
-        <Text style={styles.developmentModeText}>
-          Development mode is enabled, your app will be slower but you can use useful development
-          tools. {learnMoreButton}
-        </Text>
-      );
-    } else {
-      return (
-        <Text style={styles.developmentModeText}>
-          You are not in development mode, your app will run at full speed.
-        </Text>
-      );
-    }
-  }
-
-  _handleLearnMorePress = () => {
-    WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
-  };
-
-  _handleHelpPress = () => {
-    WebBrowser.openBrowserAsync(
-      'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
-    );
-  };
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
   },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
-  contentContainer: {
-    paddingTop: 30,
-  },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
+  image: {
+    width: '100%',
+    height: '100%',
+    borderRadius: 10,
   },
 });
