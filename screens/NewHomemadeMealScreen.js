b668e990b29469d159834aa3b5a31573c193f832
nadideozturk <nadidebaca@gmail.com>
2019-10-22 01:19:14 -0700

    Add page form initial version temp

--- screens/NewHomemadeMealScreen.js
+++ screens/NewHomemadeMealScreen.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import {
-  View,
+  Container,
+  Content,
+  Form,
+  Item,
+  Input,
+  Button,
   Text,
-} from 'react-native';
+  Spinner,
+} from 'native-base';
+import {
+  Formik,
+} from 'formik';
 
 export default class NewHomemadeMealScreen extends React.Component {
   static navigationOptions = {
@@ -11,9 +20,59 @@ export default class NewHomemadeMealScreen extends React.Component {
 
   render() {
     return (
-      <View>
-        <Text>New meal screen</Text>
-      </View>
+      <Container>
+        <Content>
+          <Formik
+            initialValues={{ mealName: '', photoUrl: '' }}
+            validate={() => {
+              const errors = {};
+              // if (!values.mealName) {
+              //   errors.namealNameme = 'Required';
+              // }
+              // if (!values.photoUrl) {
+              //   errors.photoUrl = '';
+              // }
+              return errors;
+            }}
+            onSubmit={(values, actions) => {
+              setTimeout(() => {
+                alert(JSON.stringify(values));
+                actions.setSubmitting(false);
+              }, 2000);
+            }}
+          >
+            {(props) => (
+              <Form>
+                <Item>
+                  <Input
+                    placeholder="Meal name"
+                    onBlur={props.handleBlur('mealName')}
+                    onChangeText={props.handleChange('mealName')}
+                    value={props.values.mealName}
+                  />
+                </Item>
+                <Item last>
+                  <Input
+                    placeholder="Duration"
+                    onBlur={props.handleBlur('duration')}
+                    onChangeText={props.handleChange('duration')}
+                    value={props.values.duration}
+                  />
+                </Item>
+                {props.isSubmitting ? (
+                  <Spinner color="red" />
+                ) : (
+                  <Button
+                    onPress={props.handleSubmit}
+                  >
+                    <Text>Create</Text>
+                  </Button>
+                )}
+              </Form>
+            )}
+          </Formik>
+        </Content>
+      </Container>
     );
   }
 }
