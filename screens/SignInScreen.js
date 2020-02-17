c5b10d88a413f74dee513acfe343a4e9304e4c5d
nadideozturk <nadidebaca@gmail.com>
2020-02-17 01:49:41 -0800

    Add Spinner to SignIn screen

--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -5,13 +5,23 @@ import {
   Content,
   Button,
   Text,
+  Spinner,
 } from 'native-base';
 import * as Google from 'expo-google-app-auth';
 import Config from '../config.json';
 import { navigationShape } from '../constants/Shapes';
 
 export default class SignInScreen extends React.Component {
-  _signInAsync = async () => {
+  constructor(props, state) {
+    super(props, state);
+    this.state = {
+      inProgress: false,
+    };
+    this.signInAsync = this.signInAsync.bind(this);
+  }
+
+  signInAsync = async () => {
+    this.setState({ inProgress: true });
     const { navigation } = this.props;
 
     const config = {
@@ -19,21 +29,27 @@ export default class SignInScreen extends React.Component {
       iosClientId: Config.iosClientId,
     };
 
-    // First- obtain access token from Expo's Google API
-    const { type, idToken } = await Google.logInAsync(config);
+    // without setTimeout, there is a race condition between await
+    // Google.logInAsync and react rendering which freezes the screen
+    // for about one second, until Google SSO page opens
+    setTimeout(async () => {
+      // First- obtain access token from Expo's Google API
+      const { type, idToken } = await Google.logInAsync(config);
 
-    // console.log("user object is " + JSON.stringify(user));
-    // console.log("logInAsync response: " + JSON.stringify(test));
+      // console.log("user object is " + JSON.stringify(user));
+      // console.log("logInAsync response: " + JSON.stringify(test));
 
-    if (type === 'success') {
-      await AsyncStorage.setItem('idToken', idToken);
-      navigation.navigate('App');
+      if (type === 'success') {
+        await AsyncStorage.setItem('idToken', idToken);
+        navigation.navigate('App');
 
-      // Then you can use the Google REST API
-      // let userInfoResponse = await fetch('https://www.googleapis.com/userinfo/v2/me', {
-      //   headers: { Authorization: `Bearer ${accessToken}` },
-      // });
-    }
+        // Then you can use the Google REST API
+        // let userInfoResponse = await fetch('https://www.googleapis.com/userinfo/v2/me', {
+        //   headers: { Authorization: `Bearer ${accessToken}` },
+        // });
+      }
+      this.setState({ inProgress: false });
+    }, 50);
   };
 
   static navigationOptions = {
@@ -41,6 +57,8 @@ export default class SignInScreen extends React.Component {
   };
 
   render() {
+    const { inProgress } = this.state;
+
     return (
       <Container
         style={{
@@ -49,20 +67,44 @@ export default class SignInScreen extends React.Component {
           alignItems: 'center',
         }}
       >
-        <Content>
-          <Image
-            source={
-                // eslint-disable-next-line global-require
-                require('../assets/images/logo.png')
-            }
-            fadeDuration={0}
-            style={{ width: 260, height: 260, paddingTop: 450 }}
-          />
-          {/* eslint-disable-next-line no-underscore-dangle */}
-          <Button style={{ backgroundColor: '#fef2f2', justifyContent: 'center', marginBottom: 10 }} light onPress={this._signInAsync}><Text>Sign in with Google</Text></Button>
-          <Button style={{ backgroundColor: '#fef2f2', justifyContent: 'center', marginBottom: 10 }} light><Text>Sign in with Facebook</Text></Button>
-          <Button style={{ justifyContent: 'center' }} dark transparent><Text>Maybe later</Text></Button>
-        </Content>
+        {inProgress
+          ? (
+            <Content
+              contentContainerStyle={{
+                alignItems: 'center',
+                justifyContent: 'center',
+                flex: 1,
+              }}
+            >
+              <Spinner color="red" />
+            </Content>
+          )
+          : (
+            <Content>
+              <Image
+                source={
+                    // eslint-disable-next-line global-require
+                    require('../assets/images/logo.png')
+                }
+                fadeDuration={0}
+                style={{ width: 260, height: 260, paddingTop: 450 }}
+              />
+              {/* eslint-disable-next-line no-underscore-dangle */}
+              <Button
+                style={{ backgroundColor: '#fef2f2', justifyContent: 'center', marginBottom: 10 }}
+                light
+                onPress={this.signInAsync}
+              >
+                <Text>Sign in with Google</Text>
+              </Button>
+              <Button style={{ backgroundColor: '#fef2f2', justifyContent: 'center', marginBottom: 10 }} light>
+                <Text>Sign in with Facebook</Text>
+              </Button>
+              <Button style={{ justifyContent: 'center' }} dark transparent>
+                <Text>Maybe later</Text>
+              </Button>
+            </Content>
+          )}
       </Container>
     );
   }
