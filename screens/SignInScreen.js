011f1b9d72c22da7fba3e20f56056ae6eb71e1e3
nadideozturk <nadidebaca@gmail.com>
2020-02-17 00:21:33 -0800

    SignIn screen redesign

--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { AsyncStorage, Image } from 'react-native';
 import {
-  AsyncStorage,
-  View,
+  Container,
+  Content,
   Button,
-} from 'react-native';
+  Text,
+} from 'native-base';
 import * as Google from 'expo-google-app-auth';
 import Config from '../config.json';
 import { navigationShape } from '../constants/Shapes';
@@ -35,15 +37,33 @@ export default class SignInScreen extends React.Component {
   };
 
   static navigationOptions = {
-    title: 'Please sign in',
+    header: null,
   };
 
   render() {
     return (
-      <View>
-        {/* eslint-disable-next-line no-underscore-dangle */}
-        <Button title="Sign in!" onPress={this._signInAsync} />
-      </View>
+      <Container
+        style={{
+          backgroundColor: '#F5D97E',
+          height: '100%',
+          alignItems: 'center',
+        }}
+      >
+        <Content>
+          <Image
+            source={
+                // eslint-disable-next-line global-require
+                require('../assets/images/logo.png')
+            }
+            fadeDuration={0}
+            style={{ width: 260, height: 260, paddingTop: 450 }}
+          />
+          {/* eslint-disable-next-line no-underscore-dangle */}
+          <Button style={{ backgroundColor: '#fef2f2', justifyContent: 'center', marginBottom: 10 }} light onPress={this._signInAsync}><Text>Sign in with Google</Text></Button>
+          <Button style={{ backgroundColor: '#fef2f2', justifyContent: 'center', marginBottom: 10 }} light><Text>Sign in with Facebook</Text></Button>
+          <Button style={{ justifyContent: 'center' }} dark transparent><Text>Maybe later</Text></Button>
+        </Content>
+      </Container>
     );
   }
 }
